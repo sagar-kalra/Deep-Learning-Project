2fd8aa5762d0246693d03c0b4ae938775ae8af46
Sagar Kalra <sagar.kalra98@hotmail.com>
2018-02-16 14:26:07 +0530

    Update code

--- code.py
+++ code.py
@@ -1,35 +1,48 @@
 import os
 from PIL import Image
+import cv2
+import numpy as np
 
-labels_train = os.listdir('./Training')
-labels_train = sorted(labels_train)
-print(labels_train)
+dir_train = os.listdir('./Training')
+dir_train = sorted(dir_train)
 images_train = []
 temp=[]
-for filename in labels_train:
+images_test =[]
+temp_img_train = []
+temp_img_test = []
+temp_label_train = []
+temp_label_test = []
+for filename in dir_train:
     temp = os.listdir('./Training/{}'.format(filename))
-    temp_img=[]
     for img in temp:
         img=str(img)
         allowed_extension = 'ppm'
         if (img.split('.'))[1]==allowed_extension:
             path = './Training/{}/{}'.format(filename, img)
             im = Image.open(path)
-            temp_img.append(im)
-    images_train.append(temp_img)
-labels_test = os.listdir('./Testing')
-labels_test = sorted(labels_test)
-print(labels_test)
-images_test = []
-temp=[]
-for filename in labels_test:
+            temp_label_train.append(filename)
+            im = im.resize((28,28), Image.ANTIALIAS)
+            im = np.array(im)
+            im = cv2.cvtColor(im,cv2.COLOR_RGB2GRAY)
+            im.reshape(28,28,1)
+            temp_img_train.append(im)
+images_train = np.asarray(temp_img_train)
+labels_train = np.asarray(temp_label_train)
+dir_test = os.listdir('./Testing')
+dir_test = sorted(dir_test)
+for filename in dir_test:
     temp = os.listdir('./Testing/{}'.format(filename))
-    temp_img=[]
     for img in temp:
         img=str(img)
         allowed_extension = 'ppm'
         if (img.split('.'))[1]==allowed_extension:
             path = './Testing/{}/{}'.format(filename, img)
+            temp_label_test.append(filename)
             im = Image.open(path)
-            temp_img.append(im)
-    images_test.append(temp_img)
+            im = im.resize((28,28), Image.ANTIALIAS)
+            im = np.array(im)
+            im = cv2.cvtColor(im,cv2.COLOR_RGB2GRAY)
+            temp_img_test.append(im)
+images_test = np.asarray(temp_img_test)
+labels_test = np.asarray(temp_label_test)
+
